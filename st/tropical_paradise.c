e965b056cc1799d2f24294f7730dcaaa505fdd6a
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-26 16:52:48 -0300

    feat: add st colorscheme.

---
+++ st/tropical_paradise.c
@@ -0,0 +1,25 @@
+static const char *colorname[] = {
+	// Dark colors
+	[0] = "#000000", // black
+	[1] = "#ff0d00", // red
+	[2] = "#639c00", // green
+	[3] = "#ffae00", // yellow
+	[4] = "#3599af", // blue
+	[5] = "#d400a6", // magenta
+	[6] = "#00bc7f", // cyan
+	[7] = "#e3cb98", // white
+	
+	// Light colors
+	[8]  = "#190000", // background
+	[9]  = "#f57f79", // red
+	[10] = "#d4f0a5", // green
+	[11] = "#f5cf7e", // yellow
+	[12] = "#93ccd9", // blue
+	[13] = "#d696c9", // magenta
+	[14] = "#87e2c5", // cyan
+	[15] = "#fcd276", // foreground
+};
+
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 8;
+
