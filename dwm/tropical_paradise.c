f9bb50c380c27d22e2a39827a50a07a8ae5aa233
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-28 11:01:10 -0300

    chore: adjust background color of dwm.

--- dwm/tropical_paradise.c
+++ dwm/tropical_paradise.c
@@ -1,5 +1,5 @@
-static const char col_gray1[]       = "#3a3525";
-static const char col_gray2[]       = "#3a3525";
+static const char col_gray1[]       = "#190000";
+static const char col_gray2[]       = "#190000";
 static const char col_gray3[]       = "#e1d1ad";
 static const char col_gray4[]       = "#3a3525";
 static const char col_cyan[]        = "#ff0d00";
