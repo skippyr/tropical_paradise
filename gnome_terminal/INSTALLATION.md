a4cee1960fee89f42caabc07bfcd9a1895aab97e
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-09 17:04:49 -0300

    feat: finish dark color scheme script for gnome terminal

--- gnome_terminal/INSTALLATION.md
+++ gnome_terminal/INSTALLATION.md
@@ -1,3 +1,5 @@
 ## Installation Instructions For Gnome Terminal
++ install `dconf-cli`. Some distributions that use Gnome have it already
+  pre-installed.
 + run the script of the desired color scheme that is in the `gnome_terminal`
   directories.
--- gnome_terminal/tropical_paradise_dark.sh
+++ gnome_terminal/tropical_paradise_dark.sh
@@ -15,6 +15,18 @@ apply_value() {
   gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile_uuid}/ ${key} ${value}
 }
 
+create_palette_string() {
+  local -r last_color_index=$((${#@} - 1))
+  local color_index=0
+  echo -n "["
+  for color in $@; do
+    echo -n \"${color}\"
+    [[ ! ${color_index} -eq ${last_color_index} ]] && echo -n ,
+    ((color_index++))
+  done
+  echo -n "]"
+}
+
 main() {
   # Black
   local -r dark_black=#140000
@@ -43,10 +55,37 @@ main() {
   # Cyan
   local -r dark_cyan=#02d5b5
   local -r light_cyan=#87e2c5
+
+  # White
+  local -r dark_white=#beb79c
+  local -r light_white=#fdebab
   
+  local -r palette=(
+    ${dark_black}
+    ${dark_red}
+    ${dark_green}
+    ${dark_yellow}
+    ${dark_blue}
+    ${dark_magenta}
+    ${dark_cyan}
+    ${dark_white}
+
+    ${light_white}
+    ${light_black}
+    ${light_red}
+    ${light_green}
+    ${light_yellow}
+    ${light_blue}
+    ${light_magenta}
+    ${light_cyan}
+    ${light_white}
+  )
+
   local -r default_profile_uuid=$(get_default_profile_uuid)
 
   apply_value ${default_profile_uuid} background-color "${dark_black}"
+  apply_value ${default_profile_uuid} foreground-color "${light_white}"
+  apply_value ${default_profile_uuid} palette $(create_palette_string ${palette[@]})
 }
 
 main
