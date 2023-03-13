c7b8309be01b3201f9591fa836916007fa32adfc
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-13 18:50:09 +0000

    feat: add color scheme for dwm

---
+++ dwm/tropical_paradise_dark.c
@@ -0,0 +1,17 @@
+/**
+ * name: Tropical Paradise Dark
+ * author: Sherman Rofeman (https://github.com/skippyr)
+ * license: MIT
+ * upstream: https://raw.githubusercontent.com/skippyr/tropical_paradise/main/dwm/tropical_paradise_dark.c
+ */
+// Background Color Of Workspaces
+static const char col_gray1[]       = "#140000"; // Dark Black
+// Border Color Of Workspaces
+static const char col_gray2[]       = "#140000"; // Dark Black
+// Foreground Color Of Workspaces
+static const char col_gray3[]       = "#fdebab"; // Light White
+// Foreground Color Of Selection
+static const char col_gray4[]       = "#140000"; // Dark Black
+// Background/Border Color Of Selection
+static const char col_cyan[]        = "#ff0d00"; // Dark Red
+
---
+++ dwm/tropical_paradise_light.c
@@ -0,0 +1,17 @@
+/**
+ * name: Tropical Paradise Light
+ * author: Sherman Rofeman (https://github.com/skippyr)
+ * license: MIT
+ * upstream: https://raw.githubusercontent.com/skippyr/tropical_paradise/main/dwm/tropical_paradise_light.c
+ */
+// Background Color Of Workspaces
+static const char col_gray1[]       = "#fdebab"; // Light White
+// Border Color Of Workspaces
+static const char col_gray2[]       = "#fdebab"; // Light White
+// Foreground Color Of Workspaces
+static const char col_gray3[]       = "#140000"; // Dark Black
+// Foreground Color Of Selection
+static const char col_gray4[]       = "#fdebab"; // Light White
+// Background/Border Color Of Selection
+static const char col_cyan[]        = "#c62b00"; // Dark Red
+
