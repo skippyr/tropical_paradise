6fafb58fa00c964cc3c3a2350b89ca756a174fbd
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-14 07:11:07 -0300

    docs: add comments to bspwm

--- bspwm/tropical_paradise_dark.sh
+++ bspwm/tropical_paradise_dark.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#ff0d00"
-bspc config normal_border_color "#140000"
+# name: Tropical Paradise Dark
+# author: Sherman Rofeman (https://github.com/skippyr)
+# license: MIT
+# upstream: https://raw.githubusercontent.com/skippyr/tropical_paradise/main/bspwm/tropical_paradise_dark.sh
+
+bspc config focused_border_color "#ff0d00" # Dark Red
+bspc config normal_border_color "#140000" # Dark Black
 
--- bspwm/tropical_paradise_light.sh
+++ bspwm/tropical_paradise_light.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#c62b00"
-bspc config normal_border_color "#fdebab"
+# name: Tropical Paradise Light
+# author: Sherman Rofeman (https://github.com/skippyr)
+# license: MIT
+# upstream: https://raw.githubusercontent.com/skippyr/tropical_paradise/main/bspwm/tropical_paradise_light.sh
+
+bspc config focused_border_color "#c62b00" # Dark Red
+bspc config normal_border_color "#fdebab" # Light White
 
